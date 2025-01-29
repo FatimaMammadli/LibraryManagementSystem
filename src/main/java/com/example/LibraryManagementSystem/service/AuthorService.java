16782381e1bce26de518502fc09ae07325f44d7a
FatimaMammadli <mammadlifatima068@gmail.com>
2025-01-29 21:13:12 +0400

    solving author problems about thymeleaf

--- src/main/java/com/example/LibraryManagementSystem/service/AuthorService.java
+++ src/main/java/com/example/LibraryManagementSystem/service/AuthorService.java
@@ -26,4 +26,7 @@ public class AuthorService {
         authorRepository.deleteById(id);
     }
 
+    public Author findById(Long id) {
+        return authorRepository.findById(id).orElse(null);
+    }
 }
